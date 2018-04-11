ee50bd8f4c8ec9b4c3d5ae37c63811ec20268ca5
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2018-04-11 20:20:37 +0300

    Don't compress traj data 'cuz all compressors sux

--- launch_windows.sh
+++ launch_windows.sh
@@ -5,10 +5,10 @@ trap 'kill -HUP 0' EXIT
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-sdir=sessions/`date +"%Y%m%dT%H%m%S"`
+sdir=sessions/`date +"%Y-%m-%dT%H-%M-%S"`
 mkdir $sdir
 ./wslog > $sdir/trajlog.jsons &
-./pupil_logger.py > $sdir/pupil.jsons &
+./pupil_logger.py |gzip > $sdir/pupil.jsons.gz &
 
 "/mnt/c/pupil_v1.6.11_windows_x64/pupil_capture_windows_x64_v1.6.11/pupil_capture.exe" &
 "/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe" --allow-file-access-from-files --user-data-dir=chromium-data "file:/C:/Users/TRU/webtrajsim/index.html?disableDefaultLogger=true&experiment=blindPursuit18&wsLogger=ws://localhost:8080&targetSize=0.5"
