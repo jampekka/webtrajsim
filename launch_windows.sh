a1c757a022a5e918e0eb79ed5bc63302b8093fc2
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2018-04-11 19:09:06 +0300

    Nicer launch

--- launch_windows.sh
+++ launch_windows.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # Unix sucks bad.
+set -e
 trap 'kill -HUP 0' EXIT
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-sdir=sessions/`date -Isecond |base64`
+sdir=sessions/`date +"%Y%m%dT%H%m%S"`
 mkdir $sdir
 ./wslog > $sdir/trajlog.jsons &
+./pupil_logger.py > $sdir/pupil.jsons &
 
+"/mnt/c/pupil_v1.6.11_windows_x64/pupil_capture_windows_x64_v1.6.11/pupil_capture.exe" &
 "/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe" --allow-file-access-from-files --user-data-dir=chromium-data "file:/C:/Users/TRU/webtrajsim/index.html?disableDefaultLogger=true&experiment=blindPursuit18&wsLogger=ws://localhost:8080&targetSize=0.5"
